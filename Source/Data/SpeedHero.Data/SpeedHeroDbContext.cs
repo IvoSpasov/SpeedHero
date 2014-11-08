920e7ed8fcdec2c26019062c8fbd1a4bb09c024d
ivossss <ivossss@yahoo.com>
2014-11-08 20:38:01 +0200

    Created data project and IdentityModels extracted to data

---
+++ Source/Data/SpeedHero.Data/SpeedHeroDbContext.cs
@@ -0,0 +1,18 @@
+﻿namespace SpeedHero.Data
+{
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using SpeedHero.Data.Models;
+
+    public class SpeedHeroDbContext : IdentityDbContext<User>
+    {
+        public SpeedHeroDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static SpeedHeroDbContext Create()
+        {
+            return new SpeedHeroDbContext();
+        }
+    }
+}
