4d5089e711b1d910b21dc2598ae2bee4c97ae12e
ivossss <ivossss@yahoo.com>
2014-11-09 21:30:14 +0200

    Added repository interfaces and classes that implement them.

---
+++ Source/Data/SpeedHero.Data.Common/Repository/DeletableEntityRepository.cs
@@ -0,0 +1,26 @@
+﻿namespace SpeedHero.Data.Common.Repository
+{
+    using SpeedHero.Data.Common.Models;
+    using System.Data.Entity;
+    using System.Linq;
+
+
+    public class DeletableEntityRepository<T> : GenericRepository<T>, IDeletableEntityRepository<T>
+        where T : class, IDeletableEntity
+    {
+        public DeletableEntityRepository(DbContext context)
+            : base(context)
+        {
+        }
+
+        public override IQueryable<T> All()
+        {
+            return base.All().Where(x => !x.IsDeleted);
+        }
+
+        public IQueryable<T> AllWithDeleted()
+        {
+            return base.All();
+        }
+    }
+}
