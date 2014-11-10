099ffed090737b8d350ec98056dc5d2ba645bbc1
ivossss <ivossss@yahoo.com>
2014-11-10 22:08:18 +0200

    All classes from AccountViewModels.cs and ManageViewModels.cs extracted into separate .cs files

---
+++ Source/Web/SpeedHero.Web/ViewModels/Account/ExternalLoginListViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace SpeedHero.Web.ViewModels.Account
+{
+    public class ExternalLoginListViewModel
+    {
+        public string ReturnUrl { get; set; }
+    }
+}
---
+++ Source/Web/SpeedHero.Web/ViewModels/Manage/FactorViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace SpeedHero.Web.ViewModels.Manage
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
