75afe478e2ad83a6192eb3a4a5bca41d151c03a7
ivossss <ivossss@yahoo.com>
2014-11-13 22:15:03 +0200

    KendoUI installed. Administration area created. Bundles fixed.

---
+++ Source/Web/SpeedHero.Web/Areas/AdministrationArea/AdministrationAreaAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace SpeedHero.Web.Areas.AdministrationArea
+{
+    public class AdministrationAreaAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "AdministrationArea";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "AdministrationArea_default",
+                "AdministrationArea/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Source/Web/SpeedHero.Web/Areas/AdministrationArea/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿namespace SpeedHero.Web.Areas.AdministrationArea.Controllers
+{
+    using System.Web.Mvc;
+
+    public class HomeController : Controller
+    {
+        public ActionResult Navigation()
+        {
+            return View();
+        }
+    }
+}
