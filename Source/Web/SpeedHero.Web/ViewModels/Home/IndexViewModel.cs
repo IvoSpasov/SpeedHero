12cc9691f6ee29cd3cb29dfe6403c89787097e56
ivossss <ivossss@yahoo.com>
2015-10-05 16:08:46 +0300

    Removed unnecessary attribute

--- Source/Web/SpeedHero.Web/ViewModels/Home/IndexViewModel.cs
+++ Source/Web/SpeedHero.Web/ViewModels/Home/IndexViewModel.cs
@@ -1,14 +1,12 @@
 ﻿namespace SpeedHero.Web.ViewModels.Home
 {
     using System;
-    using System.Web.Mvc;
 
     using SpeedHero.Data.Models;
     using SpeedHero.Web.Infrastructure.Mapping;
 
     public class IndexViewModel : IMapFrom<Post>
     {
-        [HiddenInput(DisplayValue = false)]
         public int Id { get; set; }
 
         public string Title { get; set; }
