06d4961d3c1ad745478825c97d25335e834498f2
ivossss <ivossss@yahoo.com>
2015-10-08 15:42:41 +0300

    Added administrator user with administration role in seed method.

--- Source/Data/SpeedHero.Data/Migrations/Configuration.cs
+++ Source/Data/SpeedHero.Data/Migrations/Configuration.cs
@@ -1,6 +1,11 @@
 namespace SpeedHero.Data.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using SpeedHero.Common;
+    using SpeedHero.Data.Models;
     using System.Data.Entity.Migrations;
+    using System.Linq;
 
     public sealed class Configuration : DbMigrationsConfiguration<SpeedHeroDbContext>
     {
@@ -25,6 +30,27 @@ namespace SpeedHero.Data.Migrations
             //     new Person { FullName = "Brice Lambson" },
             //     new Person { FullName = "Rowan Miller" }
             //   );
+
+            this.SeedRoles(context);
+            this.SeedUsersWithRoles(context);
+        }
+
+        protected void SeedRoles(SpeedHeroDbContext context)
+        {
+            context.Roles.AddOrUpdate(r => r.Name, new IdentityRole(GlobalConstants.AdministratorRoleName));
+        }
+
+        protected void SeedUsersWithRoles(SpeedHeroDbContext context)
+        {
+            var userStore = new UserStore<User>(context);
+            var userManager = new UserManager<User>(userStore); // to access user table but in different way than context
+
+            if (!context.Users.Any(u => u.UserName == "admin@admin.com"))
+            {
+                var user = new User { UserName = "admin@admin.com", Email = "admin@admin.com" };
+                userManager.Create(user, "123456");
+                userManager.AddToRole(user.Id, GlobalConstants.AdministratorRoleName);
+            }
         }
     }
 }
