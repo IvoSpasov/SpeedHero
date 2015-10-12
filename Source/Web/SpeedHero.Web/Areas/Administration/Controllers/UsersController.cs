9ea1b616fe1ac2e715ca274f1ae1ffd562188386
Ivo Spasov <ivossss@yahoo.com>
2015-10-12 17:46:25 +0300

    Users grid added in administration. Read functionality is added.

---
+++ Source/Web/SpeedHero.Web/Areas/Administration/Controllers/UsersController.cs
@@ -0,0 +1,47 @@
+﻿namespace SpeedHero.Web.Areas.Administration.Controllers
+{
+
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    using AutoMapper.QueryableExtensions;
+    using Kendo.Mvc.Extensions;
+    using Kendo.Mvc.UI;
+
+    using SpeedHero.Data.Common.Repositories;
+    using SpeedHero.Data.Models;
+    using SpeedHero.Web.Areas.Administration.Controllers.Base;
+    using SpeedHero.Web.Areas.Administration.ViewModels.Users;
+
+    public class UsersController : AdminController
+    {
+        private readonly IDeletableEntityRepository<User> usersRepository;
+
+        public UsersController(IDeletableEntityRepository<User> deletableEntityRepository)
+        {
+            this.usersRepository = deletableEntityRepository;
+        }
+
+        // GET: Administration/Users
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Read([DataSourceRequest]DataSourceRequest request)
+        {
+            var posts = this.usersRepository
+                .All()
+                .Project()
+                .To<ShowUsersViewModel>();
+
+            DataSourceResult result = posts.ToDataSourceResult(request);
+
+            return this.Json(result);
+        }
+    }
+}
---
+++ Source/Web/SpeedHero.Web/Areas/Administration/ViewModels/Users/ShowUsersViewModel.cs
@@ -0,0 +1,29 @@
+﻿namespace SpeedHero.Web.Areas.Administration.ViewModels.Users
+{
+    using SpeedHero.Data.Models;
+    using SpeedHero.Web.Areas.Administration.ViewModels.Base;
+    using SpeedHero.Web.Infrastructure.Mapping;
+
+    public class ShowUsersViewModel : AdministrationViewModel, IMapFrom<User>
+    {
+        public string Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public bool EmailConfirmed { get; set; }
+
+        public string PasswordHash { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public bool PhoneNumberConfirmed { get; set; }
+
+        public int AccessFailedCount { get; set; }
+    }
+}
