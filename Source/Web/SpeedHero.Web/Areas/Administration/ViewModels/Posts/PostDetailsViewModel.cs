23b2a62f584bdf0d3b37439142bedc9dbd693347
Ivo Spasov <ivossss@yahoo.com>
2015-10-14 18:52:36 +0300

    More details added in Details view

--- Source/Web/SpeedHero.Web/Areas/Administration/ViewModels/Posts/PostDetailsViewModel.cs
+++ Source/Web/SpeedHero.Web/Areas/Administration/ViewModels/Posts/PostDetailsViewModel.cs
@@ -1,11 +1,13 @@
 ﻿namespace SpeedHero.Web.Areas.Administration.ViewModels.Posts
 {
+    using System.ComponentModel.DataAnnotations;
+
     using AutoMapper;
 
     using SpeedHero.Data.Models;
     using SpeedHero.Web.Infrastructure.Mapping;
 
-    public class PostDetailsViewModel: IMapFrom<Post>, IHaveCustomMappings
+    public class PostDetailsViewModel : IMapFrom<Post>, IHaveCustomMappings
     {
         public int Id { get; set; }
 
@@ -13,13 +15,15 @@
 
         public string Content { get; set; }
 
+        [Display(Name = "Cover photo path")]
         public string CoverPhotoPath { get; set; }
 
+        [Display(Name = "Author name")]
         public string AuthorName { get; set; }
 
         public void CreateMappings(IConfiguration configuration)
         {
-            Mapper.CreateMap<Post, ShowPostsViewModel>()
+            Mapper.CreateMap<Post, PostDetailsViewModel>()
                 .ForMember(dto => dto.AuthorName, opt => opt.MapFrom(p => p.Author.UserName));
         }
     }
