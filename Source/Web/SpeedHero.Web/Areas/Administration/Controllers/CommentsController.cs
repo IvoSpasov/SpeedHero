5835bcf7fcc7df82ad50a1f3cd70575f5deb340f
Ivo Spasov <ivossss@yahoo.com>
2015-10-16 18:59:53 +0300

    Added functionality to show comments for post is details view.

---
+++ Source/Web/SpeedHero.Web/Areas/Administration/Controllers/CommentsController.cs
@@ -0,0 +1,38 @@
+﻿namespace SpeedHero.Web.Areas.Administration.Controllers
+{
+    using System.Linq;
+    using System.Web.Mvc;
+
+    using AutoMapper.QueryableExtensions;
+
+    using SpeedHero.Data.Common.Repositories;
+    using SpeedHero.Data.Models;
+    using SpeedHero.Web.Areas.Administration.ViewModels.Comments;
+
+    public class CommentsController : Controller
+    {
+        private IDeletableEntityRepository<Comment> commentsRepository;
+
+        public CommentsController(IDeletableEntityRepository<Comment> commentsDeletableRepository)
+        {
+            this.commentsRepository = commentsDeletableRepository;
+        }
+
+        // GET: Administration/Comments
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ShowComments(int? id)
+        {
+            var comments = this.commentsRepository
+                .All()
+                .Where(c => c.PostId == id.Value)
+                .Project()
+                .To<ShowCommentsViewModel>();
+
+            return PartialView("_ShowComments", comments);
+        }
+    }
+}
---
+++ Source/Web/SpeedHero.Web/Areas/Administration/ViewModels/Comments/ShowCommentsViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SpeedHero.Web.Areas.Administration.ViewModels.Comments
+{
+    using SpeedHero.Data.Models;
+    using SpeedHero.Web.Infrastructure.Mapping;
+
+    public class ShowCommentsViewModel : IMapFrom<Comment>
+    {
+        public string Content { get; set; }
+    }
+}
